0546aaef99c618b789ffad03749162ba57090761
Ritvik Kolhe <ritvikkolhe3@gmail.com>
2021-04-19 20:08:36 -0400

    Updated Servo motor code

--- esp32feather/main/uart_events_example_main.c
+++ esp32feather/main/uart_events_example_main.c
@@ -17,6 +17,77 @@
 
 #include "project.h"
 
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
+
+//You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
+#define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE 90       //Maximum angle in degree upto which servo can rotate
+
+static void mcpwm_example_gpio_initialize()
+{
+    printf("initializing mcpwm servo control gpio......\n");
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 21); //Set GPIO 21 as PWM0A, to which servo is connected
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, 27);
+}
+
+/**
+ * @brief Use this function to calcute pulse width for per degree rotation
+ *
+ * @param  degree_of_rotation the angle in degree to which servo has to rotate
+ *
+ * @return
+ *     - calculated pulse width
+ */
+static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
+{
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + (((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
+
+/**
+ * @brief Configure MCPWM module
+ */
+void mcpwm_servo_control(char a)
+{
+    //2. initial mcpwm configuration
+        printf("Configuring Initial Parameters of mcpwm......\n");
+        mcpwm_config_t pwm_config;
+        pwm_config.frequency = 50; //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+        pwm_config.cmpr_a = 0;     //duty cycle of PWMxA = 0
+        pwm_config.cmpr_b = 0;     //duty cycle of PWMxb = 0
+        pwm_config.counter_mode = MCPWM_UP_COUNTER;
+        pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+        mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config); //Configure PWM0A & PWM0B with above setting
+        
+    //1. mcpwm gpio initialization
+    mcpwm_example_gpio_initialize();
+
+        uint32_t angle;
+
+        if(a == "R") {
+            angle = servo_per_degree_init(0);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
+            vTaskDelay(200);
+
+            angle = servo_per_degree_init(80);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
+            vTaskDelay(500);
+        }
+
+        else {
+            angle = servo_per_degree_init(0);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, angle);
+            vTaskDelay(200);
+
+            angle = servo_per_degree_init(80);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, angle);
+            vTaskDelay(500);
+        }
+}
+
 static const char *TAG = "uart_events";
 
 /**
@@ -64,6 +135,7 @@ static void uart_event_task(void *pvParameters)
                     lcd_write_instruction_4f(0b10000000);
                     vTaskDelay(5 / portTICK_PERIOD_MS);
                     lcd_write_string_4f((uint8_t*)dtmp);
+                    mcpwm_servo_control(dtmp[0]);
                     break;
                 //Event of HW FIFO overflow detected
                 case UART_FIFO_OVF:
