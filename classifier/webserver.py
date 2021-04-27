1d9f4625d827be9ba6e0387ba0a2406d30a339c7
kristerseiya <>
2021-04-27 03:16:52 -0400

    commented background removal

--- classifier/webserver.py
+++ classifier/webserver.py
@@ -4,11 +4,10 @@ import io
 import torch
 from torchvision import models
 import torchvision.transforms as transforms
+import torch.nn.functional as F
 from PIL import Image
 from flask import Flask, jsonify, request
 from flask import render_template, Response
-import png
-import cv2
 import numpy as np
 
 import model
@@ -19,23 +18,17 @@ import data
 app = Flask(__name__)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-net = model.WasteNet(10).move(device)
+classifier = model.WasteNet(9).move(device)
 # net.load_state_dict(torch.load('model/mar31_1103.pth', map_location=device))
-net.load_state_dict(torch.load('model/apr23_2334.pth', map_location=device))
-net.eval()
+# classifier.load_state_dict(torch.load('model/apr23_2334.pth', map_location=device))
+classifier.load_state_dict(torch.load('model/wastenet.pth', map_location=device))
+classifier.eval()
+bgremover = model.U2NET()
+bgremover.load_state_dict(torch.load('model/u2net.pth', map_location=device))
+bgremover.eval()
 
-def bytes2PILImage(image_bytes):
-    return Image.open(io.BytesIO(image_bytes)).convert('RGB')
-
-def transform_image(pil_image):
-    my_transforms = data.get_transform('test')
-    return my_transforms(pil_image).unsqueeze(0)
-
-def get_prediction(pil_image):
-    tensor = transform_image(pil_image)
-    outputs = net(tensor)
-    predict = torch.argmax(outputs, -1).item()
-    return data.id_label(predict)
+b = torch.zeros(1, 3, 320, 320, device=device)
+b = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(b)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -43,10 +36,29 @@ def predict():
         global img_bytes
         global class_name
         img_bytes = request.data
-
-        img = bytes2PILImage(img_bytes)
-        class_name = get_prediction(img)
-        return class_name
+        img = Image.open(io.BytesIO(img_bytes)).convert('RGB')
+        # img.show()
+        x = transforms.Resize(320)(img)
+        x = transforms.CenterCrop(320)(x)
+        x = transforms.ToTensor()(x)
+        x = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(x)
+        x = x.unsqueeze(0)
+        x = x.to(device)
+        # mask, _, _, _, _, _, _ = bgremover(x)
+        # mask = mask[:, 0, :, :]
+        # x = x * mask + b * (1 - mask)
+        # tmp = x.detach().cpu().numpy()
+        # tmp = tmp[0]
+        # tmp[0, :, :] = tmp[0, :, :] * 0.229 + 0.485
+        # tmp[1, :, :] = tmp[1, :, :] * 0.224 + 0.456
+        # tmp[2, :, :] = tmp[2, :, :] * 0.225 + 0.406
+        # tmp = tmp.transpose([1, 2, 0])
+        # tmp = Image.fromarray((tmp*255).astype(np.uint8)).convert('RGB')
+        # tmp.show()
+        x = F.interpolate(x, size=[224, 224])
+        output = classifier(x)
+        predict = torch.argmax(output, -1).item()
+        return data.id_label(predict)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True)
